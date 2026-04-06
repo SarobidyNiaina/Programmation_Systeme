59a60d509cb7a9114db22d297de290a8619395bd
Maminiaina Sarobidy <bidymamy8@gmail.com>
2026-04-07 00:41:10 +0300

    Update E31.c

--- C/Chap3_exec/E31.c
+++ C/Chap3_exec/E31.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 int  main(int argc, char**argv)
-{
-	system("ls");
+{   
+	int a, b;
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    printf("la somme est %d\n", a + b)
 	return  0;
 }
