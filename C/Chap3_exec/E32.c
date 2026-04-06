bbecd20bc6699f2a34e21cc0a426357ef7431db8
Maminiaina Sarobidy <bidymamy8@gmail.com>
2026-04-07 00:46:06 +0300

    Refactor command execution to use execlp
    Replace system call with execlp for better security and error handling.

--- C/Chap3_exec/E32.c
+++ C/Chap3_exec/E32.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int  main(int argc, char*argv[])
+
+int main(int argc, char *argv[])
 {
-	char commande[200];
-	
-	snprintf(commande, sizeof(commande),"cp -r * %s", argv[1]);
-	system(commande);
-	printf("execution du commande...");
-	return  0;
+    if (argc != 2)
+    {
+        printf("Usage: %s repertoire_destination\n", argv[0]);
+        exit(1);
+    }
+    printf("Copie du repertoire courant vers %s...\n", argv[1]);
+    execlp("cp", "cp", "-r", ".", argv[1], NULL);
+    perror("Erreur exec");
+    return 1;
 }
